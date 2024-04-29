111f2b8bde2ffa010482537f66805116275a9c0e
Zach <zphillips080@anselm.edu>
2024-04-28 22:01:46 -0400

    Login Changes

--- cookie.js
+++ cookie.js
@@ -1,18 +1,14 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("signIn");
+const logoutButton = document.getElementById("logout-dropdown");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.uname.value;
     const password = loginForm.psw.value;
 
-    if (username == "admin" && password == "admin") {
-        setCookie("Username", "admin", 265)
-        window.location.href = "admin.html" //admin.html
-    }
-    else {
-      alert("Failed Log In Attempt");
-    }
+    setCookie("Username", username, 265)
+    window.location.href = "index.html"
 })
 
 function setCookie(cname, cvalue, exdays) {
@@ -40,11 +36,22 @@ function getCookie(cname) {
 
 function checkCookie() {
   let user = getCookie("Username");
-  if (user != "") {
-    document.getElementById("nav-login").style.display = "none";
-    document.getElementById("nav-account").style.display = "block";
-  } else{
+  if (user == "") {
     document.getElementById("nav-login").style.display = "block";
     document.getElementById("nav-account").style.display = "none";
+  } else if (user == "admin"){
+    document.getElementById("nav-login").style.display = "none";
+    document.getElementById("nav-account").style.display = "block";
+    document.getElementById("logout-dropdown").style.display = "block";
+    document.getElementById("admin-dropdown").style.display = "block";
+  } else {
+    document.getElementById("nav-login").style.display = "none";
+    document.getElementById("nav-account").style.display = "block";
+    document.getElementById("logout-dropdown").style.display = "block";
+    document.getElementById("admin-dropdown").style.display = "none";
   }
+}
+
+function deleteCookie() {
+  setCookie("Username", "username", 0)
 }
